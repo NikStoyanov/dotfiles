08c28556251011bd558a6a371e84e10e96a0fc29
NikStoyanov <nstoyanov.92@gmail.com>
2018-11-05 05:18:30 +0000

    update install file for symlink and emacs

--- install.sh
+++ install.sh
@@ -1,3 +1,15 @@
+# Variables
+dotfiles = ~/git/dotfiles
+
+# Delete existing dot files and folders
+sudo rm -rf ~/.vim > /dev/null 2>&1
+sudo rm -rf ~/.vimrc > /dev/null 2>&1
+sudo rm -rf ~/.emacs.d > /dev/null 2>&1
+
+# symlink
+ln -sf $dotfiles/vimrc ~/.vimrc
+ln -sf $dotfiles/emacs ~/.emacs.d/config.el
+
 # setup vim
 echo "Setting up vim"
 
@@ -14,7 +26,24 @@ echo "Clone Vim-Plug Plugin"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
          https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo "Copying .vimrc"
-cp -f .vimrc $HOME/
-
 echo "Vim setup done"
+
+# setup emacs
+echo "Setting up emacs"
+
+if [ ! d "$HOME/.emacs.d/emacs_backup/" ]; then
+        mkdir $HOME/.emacs.d/emacs_backup
+fi
+
+if [ ! d "$HOME/.emacs.d/autosave/"]; then
+        mkdir $HOME/.emacs.d/autosave/
+fi
+
+if [ ! d "$HOME/.emacs.d/lisp/"]; then
+        mkdir $HOME/.emacs.d/lisp/
+fi
+
+cp $dotfiles/lisp/ox-rss.el $HOME/.emacs.d/lisp/ox-rss.el
+cp $dotfiles/lisp/ox-word.el $HOME/.emacs.d/lisp/ox-word.el
+
+echo "Emacs setup done"
