63194116e48f7087663a713bc9990df164566726
Nik Stoyanov <nstoyanov.92@gmail.com>
2018-11-05 22:00:18 +0000

    BUG: fixed symlink to correct files

--- install.sh
+++ install.sh
@@ -1,25 +1,21 @@
 # Variables
-dotfiles = ~/git/dotfiles
+dotfiles="$HOME/git/dotfiles"
 
 # Delete existing dot files and folders
-sudo rm -rf ~/.vim > /dev/null 2>&1
-sudo rm -rf ~/.vimrc > /dev/null 2>&1
-sudo rm -rf ~/.emacs.d > /dev/null 2>&1
-
-# symlink
-ln -sf $dotfiles/vimrc ~/.vimrc
-ln -sf $dotfiles/emacs ~/.emacs.d/config.el
+rm -rf "$HOME/.vim"
+rm -rf "$HOME/.vimrc"
+rm -rf "$HOME/.emacs.d"
 
 # setup vim
 echo "Setting up vim"
 
 if [ ! -d "$HOME/.vim" ]; then
-        mkdir $HOME/.vim
-        mkdir $HOME/.vim/autoload
+        mkdir "$HOME/.vim"
+        mkdir "$HOME/.vim/autoload"
 fi
 
 if [ ! -d "$HOME/.vim/autoload/" ]; then
-        mkdir $HOME/.vim/autoload
+        mkdir "$HOME/.vim/autoload"
 fi
 
 echo "Clone Vim-Plug Plugin"
@@ -31,23 +27,32 @@ echo "Vim setup done"
 # setup emacs
 echo "Setting up emacs"
 
-if [ ! d "$HOME/.emacs.d/emacs_backup/" ]; then
-        mkdir $HOME/.emacs.d/emacs_backup
+if [ ! -d "$HOME/.emacs.d/" ]; then
+	mkdir "$HOME/.emacs.d/"
 fi
 
-if [ ! d "$HOME/.emacs.d/autosave/"]; then
-        mkdir $HOME/.emacs.d/autosave/
+if [ ! -d "$HOME/.emacs.d/emacs_backup/" ]; then
+        mkdir "$HOME/.emacs.d/emacs_backup"
 fi
 
-if [ ! d "$HOME/.emacs.d/lisp/"]; then
-        mkdir $HOME/.emacs.d/lisp/
+if [ ! -d "$HOME/.emacs.d/autosave/" ]; then
+        mkdir "$HOME/.emacs.d/autosave/"
 fi
 
-cp $dotfiles/lisp/ox-rss.el $HOME/.emacs.d/lisp/ox-rss.el
-cp $dotfiles/lisp/ox-word.el $HOME/.emacs.d/lisp/ox-word.el
+if [ ! -d "$HOME/.emacs.d/lisp/" ]; then
+        mkdir "$HOME/.emacs.d/lisp/"
+fi
+
+cp "$dotfiles/emacs" "$HOME/.emacs.d/config.el"
+cp "$dotfiles/lisp/ox-rss.el" "$HOME/.emacs.d/lisp/ox-rss.el"
+cp "$dotfiles/lisp/ox-word.el" "$HOME/.emacs.d/lisp/ox-word.el"
 
 echo "Emacs setup done"
 
+# symlink
+ln -sf "$dotfiles/vimrc" "$HOME/.vimrc"
+ln -sf "$dotfiles/emacs" "$HOME/.emacs.d/config.el"
+
 # sync scripts
-cp $dotfiles/sync/gsync.sh $HOME/gsync.sh
-cp $dotfiles/sync/sync.sh $HOME/sync.sh
+cp "$dotfiles/sync/gsync.sh" "$HOME/gsync.sh"
+cp "$dotfiles/sync/sync.sh" "$HOME/sync.sh"
