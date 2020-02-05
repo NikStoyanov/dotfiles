efec06df657980f9d5c6a61ce515efce3ccf15d0
Nikola Stoyanov <nstoyanov.92@gmail.com>
2020-02-05 23:57:48 +0000

    Add default monitors

--- bin/monitor-plug.sh
+++ bin/monitor-plug.sh
@@ -40,23 +40,29 @@ if [ "$STATUS" = "disconnected" ]; then
 	dpi=144
 else
 	if [[ $1 == "mirror" ]]; then
-		/usr/bin/xrandr --output $DEV --mode 1024x768
-		/usr/bin/xrandr --output eDP-1 --mode 1024x768 --same-as $DEV
+		/usr/bin/xrandr --output $DEV --mode 1366x768
+		/usr/bin/xrandr --output eDP-1 --mode 1366x768 --same-as $DEV
 	elif [[ $1 == "single" ]]; then
 		/usr/bin/xrandr --output eDP-1 --off
 		/usr/bin/xrandr --output $DEV --mode 1920x1080
 	else
-		edid=$(/usr/bin/cat /sys/class/drm/card0-$DEVC/edid | /usr/bin/sha512sum - | /usr/bin/sed 's/\s*-$//')
+		edid=$(cat /sys/class/drm/card0-$DEVC/edid | /usr/bin/sha512sum - | sed 's/\s*-$//')
 
 		laptop_res="1366x768"
 		pos="primary --rotate normal --left-of"
 
 		case "$edid" in
 			"82bbccd669dc0773604295b051d8339382d8544cab289ec0a4b3fe874d8d5796590fcf46a9ef8b1dc5755b0c0f07806d0c975fc2b755356eedc5720b48a2ba57")
+				post="primary --rotate normal"
+				/usr/bin/xrandr --output eDP-1 --off
+				/usr/bin/xrandr --output $DEV --mode 1920x1080 --$pos
+				;;
+			"cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e")
+				post="primary --rotate normal"
+				/usr/bin/xrandr --output eDP-1 --off
+				/usr/bin/xrandr --output $DEV --mode 1920x1080 --$pos
 				;;
 		esac
-
-		/usr/bin/xrandr --output eDP-1 --mode "$laptop_res" --output $DEV --$pos eDP-1 --auto
 		sleep 1
 	fi
 fi
