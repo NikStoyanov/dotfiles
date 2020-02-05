bc5b9ac839d4529e037ec5769d6e648df1f79a58
Nikola Stoyanov <nstoyanov.92@gmail.com>
2020-02-05 23:52:06 +0000

    Make more vimy

--- startup.jl
+++ startup.jl
@@ -7,6 +7,7 @@ atreplinit() do repl
 
     try
         @eval using OhMyREPL
+        @eval enable_autocomplete_brackets(false)
     catch e
         @warn "error while importing OhMyREPL" e
     end
